d55f812849d1d7af14dc7f8418dd4a5604c28cc4
Daniel Boone <DanielBoone@Daniels-MacBook-Pro.local>
2015-09-11 17:15:13 -0500

    PUSH 2

--- jquery.js
+++ jquery.js
@@ -10,27 +10,39 @@ $(document).keydown(function(e) {
      
 });
        
- $(document).keyup(function(e) {
+$(document).keyup(function(e) {
      if (e.which === 16) {
     $('#keyboard-upper-container').hide();
     $('#keyboard-lower-container').show();
+    
      };
      });
 });
 
-$(document).keypress(function(e) {
-    var s = String.fromCharCode(e.which);
-    if (s.match(/[a-zA-Z\.]/))
-        console.log(s + ' is a match!');
+$(document).keypress(function(e){
+     var s = String.fromCharCode(e.which);
+     if (s.match(/[a-zA-Z]/),(/[32]/));
+       console.log(s + '');
 });
+
+$(document).keypress(function(e) {
+     var pressKey =e.charCode;
+     $('#'+pressKey).addClass('highlight') ;
+});
+
 
 /*var lines = [];
 var lineCount = 0;
 $(sentences).each(function(){
-   var letters = this.split();
+   var letters = this.split('');
+   console.log(letters);
    var line = $('<div class="letters"></div>');
    $(letters).each(function() {
-       line.append($('<div class="letter"></div>').text(this));
+       var letter = this;
+       if(letter == ' ') {
+           letter = '&nbsp;';
+       }
+       line.append($('<div class="letter"></div>').html(letter));
    });
    lines.push(line);
 });
